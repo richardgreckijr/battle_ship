51a47a3ca3ed97d2cf9dfd9bc16d8832231485ea
richardgreckijr <richardgreckijr@gmail.com>
2021-06-09 20:50:39 -0400

    location of ship is now random.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Variables to hold each cell of ship
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 //player's guess
 var userGuess;
